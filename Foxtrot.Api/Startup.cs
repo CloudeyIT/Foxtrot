375ea7ca99b38288c67c88e27b8cb5c8177a5696
Kristo Lippur <kristo@elnino.tech>
2021-08-12 00:19:55 +0200

    Fix browser download

--- Foxtrot.Api/Startup.cs
+++ Foxtrot.Api/Startup.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Threading.Tasks;
 using Foxtrot.Api.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using PuppeteerSharp;
 
@@ -22,13 +25,28 @@ namespace Foxtrot.Api
         public void ConfigureServices (IServiceCollection services)
         {
             services.AddControllers();
+            services.AddLogging();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "Foxtrot", Version = "v1"});
             });
 
-            new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
-            services.AddSingleton(_ => Puppeteer.LaunchAsync(new LaunchOptions {Headless = true}).Result);
+            services.AddSingleton(_ =>
+                {
+                    var revision = new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultChromiumRevision).Result; // Chrome 92.0.4515.131 (2021-08-02)
+                    var logger = _.GetRequiredService<ILogger<Startup>>();
+                    if (revision.Downloaded)
+                    {
+                        logger.LogInformation($"Downloaded Chrome revision {revision.Revision}");
+                    }
+                    else
+                    {
+                        logger.LogError($"Failed to download Chrome revision {revision.Revision}");
+                    }
+                    
+                    return Puppeteer.LaunchAsync(new LaunchOptions { Headless = true }).Result;
+                }
+            );
             services.AddScoped<PdfService>();
         }
 
